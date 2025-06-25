c469005b0a6fd2ed6740b7c5b6fce2a59017f559
Leticia Eto Filó Candido <160032869+Leticia-Eto@users.noreply.github.com>
2025-06-25 17:01:23 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php 
+
+ return [
+    'accesstoken' => 'TEST-1257163527376609-062413-f7088612d1843fbf7714ba8585561022-194484818',
+    'url_notification_sdk' => 'https://95d60c603db969.lhr.life/curso/sdk/notification.php',
+    'url_notification_api' => 'https://95d60c603db969.lhr.life/curso/api/notification.php'
+];
